9443ba96762a2592e63c59e11d7838eceaf2764a
Radim Vansa <rvansa@azul.com>
2025-04-23 10:03:33 +0200

    Support multiple targets (benchmark.uri.file)

--- src/main/java/org/github/crac/benchmarks/TimeToFirstReply.java
+++ src/main/java/org/github/crac/benchmarks/TimeToFirstReply.java
@@ -5,21 +5,40 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.LongAccumulator;
 import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.Collectors;
 
-public class TimeToFirstReply {
+    public class TimeToFirstReply {
     static final int WARMUP = Integer.getInteger("benchmark.warmup", 10);
     static final int MEASURED = Integer.getInteger("benchmark.measured", 40);
-    static final URI uri = URI.create(System.getProperty("benchmark.uri", "http://localhost:8080"));
+    static final List<URI> targets;
 
     static final LongAdder sum = new LongAdder();
     static final LongAccumulator min = new LongAccumulator(Math::min, Long.MAX_VALUE);
     static final LongAccumulator max = new LongAccumulator(Math::max, 0);
     static AtomicBoolean received = new AtomicBoolean();
+
+    static {
+        String filename = System.getProperty("benchmark.uri.file");
+        if (filename == null) {
+            targets = Collections.singletonList(URI.create(System.getProperty("benchmark.uri", "http://localhost:8080")));
+        } else {
+            try {
+                targets = Files.readAllLines(Path.of(filename)).stream().map(URI::create).collect(Collectors.toList());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         if (args.length == 0) {
@@ -43,27 +62,38 @@ public class TimeToFirstReply {
                 long start = System.nanoTime();
                 received.set(false);
                 while (process.isAlive() && !received.get()) {
-                    long send = System.nanoTime();
-                    client.sendAsync(HttpRequest.newBuilder(uri).GET().build(), HttpResponse.BodyHandlers.discarding()).thenAccept(response -> {
-                        long end = System.nanoTime();
-                        if (response.statusCode() < 200 || response.statusCode() > 299) {
-                            System.out.println("Invalid status: " + response.statusCode());
-                            System.exit(1);
-                        }
-                        if (!received.compareAndSet(false, true)) {
-                            return;
-                        }
-                        if (isWarmup) {
-                            System.out.print('-');
-                        } else {
-                            long diff = end - start;
-                            sum.add(diff);
-                            min.accumulate(diff);
-                            max.accumulate(diff);
-                            System.out.print('+');
-                        }
-                        System.out.flush();
-                    }).exceptionally(ex -> null).join();
+                    var future = new CompletableFuture<>();
+                    AtomicInteger responseCounter = new AtomicInteger(targets.size());
+                    for (URI uri: targets) {
+                        client.sendAsync(HttpRequest.newBuilder(uri).GET().build(), HttpResponse.BodyHandlers.discarding()).thenAccept(response -> {
+                            long end = System.nanoTime();
+                            if (response.statusCode() < 200 || response.statusCode() > 299) {
+                                System.out.println("Invalid status: " + response.statusCode());
+                                System.exit(1);
+                            }
+                            if (responseCounter.decrementAndGet() != 0) {
+                                return;
+                            }
+                            if (!received.compareAndSet(false, true)) {
+                                return;
+                            }
+                            if (isWarmup) {
+                                System.out.print('-');
+                            } else {
+                                long diff = end - start;
+                                sum.add(diff);
+                                min.accumulate(diff);
+                                max.accumulate(diff);
+                                System.out.print('+');
+                            }
+                            future.complete(null);
+                            System.out.flush();
+                        }).exceptionally(ex -> {
+                            future.completeExceptionally(ex);
+                            return null;
+                        });
+                    }
+                    future.exceptionally(ex -> null).join();
                 }
                 if (!received.get()) {
                     System.err.println("Process failed before receiving reply.");
